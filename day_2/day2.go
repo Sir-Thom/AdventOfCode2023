c81aec3af077e64f423c7c615055e7e4d2b786a5
Sir-Thom <Sir-Thom1702@pm.me>
2023-12-02 22:43:59 -0500

    day 2

--- day_2/day2.go
+++ day_2/day2.go
@@ -1,9 +1,62 @@
-package main  
-import "fmt"
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func isPossible(parts string) bool {
+	lines := strings.Split(parts, "; ")
+
+	for _, part := range lines {
+		cubes := strings.Split(part, ", ")
+		for _, cube := range cubes {
+			numColor := strings.Split(cube, " ")
+			num, _ := strconv.Atoi(numColor[0])
+			color := numColor[1]
+
+			switch color {
+			case "red":
+				if num > 12 {
+					return false
+				}
+			case "green":
+				if num > 13 {
+					return false
+				}
+			case "blue":
+				if num > 14 {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
 
 func main() {
-	fmt.Println("Day 2 of advent of code")
-	for i:=0; i <= 5; i++{
-		fmt.Println(i)
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
 	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	ans := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		gameParts := strings.Split(line, ": ")
+		game, parts := gameParts[0], gameParts[1]
+		_id, _ := strconv.Atoi(strings.Split(game, " ")[1])
+
+		if isPossible(parts) {
+			ans += _id
+		}
+	}
+
+	fmt.Println(ans)
 }
